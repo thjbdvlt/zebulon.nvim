86efc3783e6084815f7b16a1881576fd954a47f3
thjbbvlt <thibault2@ik.me>
2024-03-01 16:35:48 +0100

    modifié :         colors/zebulon.lua supprimé :        colors/zebulon_night.vim

--- colors/zebulon.lua
+++ colors/zebulon.lua
@@ -6,30 +6,40 @@ vim.g.colors_name = 'zebulon'
 local hi = vim.api.nvim_set_hl
 
 local base = {
+
     -- syntax
-    {"Normal", { ctermbg = 231, ctermfg = 016 }},
-    {"Function", { ctermfg = 19 }},
-    {"Comment", { italic = true, ctermfg = 145 }},
-    {"Statement", { ctermfg = 178 }},
-    {"Operator", { ctermfg = 160 }},
-    {"String", { ctermfg = 28 }},
-    {"Constant", { ctermfg = 30 } },
-    {"Type", { ctermfg = 163 }},
-    {"Special", { ctermfg = 200 }},
-    {"Title", { ctermfg = 163, bold = true }},
-    {"Punctuation", { ctermfg = 89 }},
+    { "Normal",      { ctermfg = 16,
+                       ctermbg = 231 }} ,
+    { "Function",    { ctermfg = 19  }},
+    { "Statement",   { ctermfg = 178 }},
+    { "Operator",    { ctermfg = 160 }},
+    { "String",      { ctermfg = 28  }},
+    { "Constant",    { ctermfg = 30  }},
+    { "Type",        { ctermfg = 163 }},
+    { "Special",     { ctermfg = 200 }},
+    { "Punctuation", { ctermfg = 89  }},
+    { "Title",       { ctermfg = 163,
+                         bold = true }},
+    { "Comment",     { ctermfg = 145,
+                       italic = true }},
 
     -- interface
-    {"Visual", {ctermbg = 255}},
-    {"Pmenu", {ctermfg = 016, ctermbg = 255}},
-    {"Cursor", {ctermbg = 212, ctermfg = 214}},
-    {"CursorLine", {}},
-    {"CursorLineNr", { ctermfg = 178 }},
-    {"MatchParen", {ctermbg = 207, ctermfg = 231}},
-    {"Error", { ctermbg = 160, ctermfg = 231 }},
-    {"TabLineFill", { ctermfg = 255  }},
-    {"LineNr", {ctermfg = 254, ctermbg = 231}},
-    {"SignColumn", { ctermbg = 231, ctermfg = 145 }},
+    { "Visual",       { ctermbg = 255 }},
+    { "Pmenu",        { ctermfg = 16,
+                        ctermbg = 255 }},
+    { "Cursor",       { ctermbg = 212,
+                        ctermfg = 214 }},
+    { "CursorLine",   {               }},
+    { "CursorLineNr", { ctermfg = 178 }},
+    { "MatchParen",   { ctermbg = 207,
+                        ctermfg = 231 }},
+    { "Error",        { ctermbg = 160,
+                        ctermfg = 231 }},
+    { "TabLineFill",  { ctermfg = 255 }},
+    { "LineNr",       { ctermfg = 254,
+                        ctermbg = 231 }},
+    { "SignColumn",   { ctermbg = 231,
+                        ctermfg = 145 }},
 }
 
 for i=1, #base do
@@ -95,7 +105,7 @@ local links = {
         "DiagnosticVirtualTextHint",
         "DiagnosticSignHint",
     },
-        Pmenu = {
+    Pmenu = {
         "Folded",
         "ModeMsg",
         "StatusLine",
