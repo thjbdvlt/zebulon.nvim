18561c2ddc9d697d44b879db223c8de0412e1fe7
thjbbvlt <thibault2@ik.me>
2024-05-06 00:34:02 +0200

    night: spellbad darker

--- colors/zebulon_night.lua
+++ colors/zebulon_night.lua
@@ -7,119 +7,124 @@ local hi = vim.api.nvim_set_hl
 
 local base = {
 
- -- syntax
- { "Normal",       { ctermfg = 231,
-                     ctermbg = 16  }},
- { "Function",     { ctermfg = 81  }},
- { "Comment",      { ctermfg = 243,
-                     italic = true }},
- { "Statement",    { ctermfg = 220 }},
- { "Operator",     { ctermfg = 196 }},
- { "String",       { ctermfg = 113 }},
- { "Constant",     { ctermfg = 85  }},
- { "Type",         { ctermfg = 207 }},
- { "Special",      { ctermfg = 170 }},
- { "Title",        { ctermfg = 163,
-                       bold = true }},
- { "Punctuation",  { ctermfg = 126 }},
-
- -- interface
- { "Visual",       { ctermbg = 235 }},
- { "Pmenu",        { ctermfg = 145,
-                     ctermbg = 234 }},
- { "Cursor",       { ctermbg = 160,
-                     ctermfg = 231 }},
- { "CursorLine",   {               }},
- { "CursorLineNr", { ctermfg = 220 }},
- { "MatchParen",   { ctermbg = 205,
-                     ctermfg = 16  }},
- { "Error",        { ctermbg = 160,
-                     ctermfg = 231 }},
- { "TabLineFill",  { ctermfg = 255 }},
- { "LineNr",       { ctermfg = 240,
-                     ctermbg = 16  }},
- { "SignColumn",   { ctermbg = 16,
-                     ctermfg = 145 }},
-
-}
-
-for i=1, #base do
-    local y = base[i]
-    hi(0, y[1], y[2])
-end
-
-local links = {
-
     -- syntax
-    Normal = {
-        "Identifier",
-        "@variable",
-        "@parameter",
-        "@field",
-        "Folded",
-    },
-    Function = {
-        "@function",
-        "@function.builtin",
-        "@function.call",
-        "@method",
-        "markdownCode",
-        "markdownCodeBlock",
-    },
-    Statement = {
-        "Include",
-        "PreProc"
-    },
-    Operator = {
-        "@operator"
-    },
-    Constant = {
-        "@constant",
-        "Boolean",
-        "@number",
-        "@character",
-        "@constant.builtin",
-    },
-    Type = {
-        "@type",
-        "@constructor",
-        "@type.builtin",
-    },
-    Punctuation = {
-        "@punctuation"
-    },
+    { "Normal",       { ctermfg = 231,
+                        ctermbg = 16  }},
+    { "Function",     { ctermfg = 81  }},
+    { "Comment",      { ctermfg = 243,
+                        italic = true }},
+    { "Statement",    { ctermfg = 220 }},
+    { "Operator",     { ctermfg = 196 }},
+    { "String",       { ctermfg = 113 }},
+    { "Constant",     { ctermfg = 85  }},
+    { "Type",         { ctermfg = 207 }},
+    { "Special",      { ctermfg = 170 }},
+    { "Title",        { ctermfg = 163,
+                        bold = true }},
+    { "Punctuation",  { ctermfg = 126 }},
 
     -- interface
-    LineNr = {
-        "NonText",
-        "WinSeparator",
-    },
-    CursorLineNr = {
-        "CursorLineSign",
-    },
-    SignColumn =  {
-        "FoldColumn",
-        "StatusLineNC",
-        "TabLine",
-        "VertSplit",
-        "NormalFloat",
-        "DiagnosticFloatingHint",
-        "DiagnosticVirtualTextHint",
-        "DiagnosticSignHint",
-    },
-    Pmenu = {
-        "ModeMsg",
-        "StatusLine",
-        "TabLineSel",
-    },
-    Error = {
-        "DiagnosticError",
-    },
-    Visual = {
-        "LspReferenceText",
-        "LspReferenceRead",
-        "LspReferenceWrite",
-    },
+    { "Visual",       { ctermbg = 235 }},
+    { "Pmenu",        { ctermfg = 145,
+                        ctermbg = 234 }},
+    { "Cursor",       { ctermbg = 160,
+                        ctermfg = 231 }},
+    { "CursorLine",   {               }},
+    { "CursorLineNr", { ctermfg = 220 }},
+    { "MatchParen",   { ctermbg = 205,
+                        ctermfg = 16  }},
+    { "Error",        { ctermbg = 52,
+                        ctermfg = 231 }},
+    { "TabLineFill",  { ctermfg = 255 }},
+    { "LineNr",       { ctermfg = 240,
+                        ctermbg = 16  }},
+    { "SignColumn",   { ctermbg = 16,
+                        ctermfg = 145 }},
+
+    { "SpellBad",   { ctermbg = 52 }},
+
+    -- plugin
+    {'LeapLabelPrimary', { ctermbg = 220, ctermfg = 30 }}
+
+    }
+
+    for i=1, #base do
+        local y = base[i]
+        hi(0, y[1], y[2])
+    end
+
+    local links = {
+
+        -- syntax
+        Normal = {
+            "Identifier",
+            "@variable",
+            "@parameter",
+            "@field",
+            "Folded",
+        },
+        Function = {
+            "@function",
+            "@function.builtin",
+            "@function.call",
+            "@method",
+            "markdownCode",
+            "markdownCodeBlock",
+        },
+        Statement = {
+            "Include",
+            "PreProc"
+        },
+        Operator = {
+            "@operator"
+        },
+        Constant = {
+            "@constant",
+            "Boolean",
+            "@number",
+            "@character",
+            "@constant.builtin",
+        },
+        Type = {
+            "@type",
+            "@constructor",
+            "@type.builtin",
+        },
+        Punctuation = {
+            "@punctuation"
+        },
+
+        -- interface
+        LineNr = {
+            "NonText",
+            "WinSeparator",
+        },
+        CursorLineNr = {
+            "CursorLineSign",
+        },
+        SignColumn =  {
+            "FoldColumn",
+            "StatusLineNC",
+            "TabLine",
+            "VertSplit",
+            "NormalFloat",
+            "DiagnosticFloatingHint",
+            "DiagnosticVirtualTextHint",
+            "DiagnosticSignHint",
+        },
+        Pmenu = {
+            "ModeMsg",
+            "StatusLine",
+            "TabLineSel",
+        },
+        Error = {
+            "DiagnosticError",
+        },
+        Visual = {
+            "LspReferenceText",
+            "LspReferenceRead",
+            "LspReferenceWrite",
+        },
 }
 
 for group, subgroups in pairs(links) do
